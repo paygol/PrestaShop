23df3add64d14fd7dacbbb1829021334deda6164
Paygol <support@paygol.com>
2017-06-02 16:23:27 -0400

    Update paygol_ipn.php

--- paygol/paygol_ipn.php
+++ paygol/paygol_ipn.php
@@ -3,7 +3,7 @@
 * Paygol.com
 *
 * @author    Paygol 
-* @copyright 2016 Paygol
+* @copyright 2017 Paygol
 * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 */
 include_once(dirname(__FILE__).'/../../config/config.inc.php');
